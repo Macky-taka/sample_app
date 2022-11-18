5cbe616bd11b897d209557a81f90918488204c6f
Macky <makino@smartworx.co.jp>
2022-11-18 11:27:09 +0000

    Finish user edit, update, index, and destroy actions

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,17 +7,19 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
   
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: "",
                                               email: "foo@invalid",
-                                              passsword: "foo",
+                                              password: "foo",
                                               password_confirmation: "bar" } }
     assert_template 'users/edit'
     assert_select "div.alert", "The form contains 4 errors."
   end
   
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "Foo Bar"
@@ -32,6 +34,42 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
+  
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              passsword: "",
+                                              password_confirmation: ""  } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+  
+  test "succcessful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    assert_equal session[:forwarding_url], edit_user_url(@user)
+    log_in_as(@user)
+    assert_nil session[:forwarding_url]
+    name = "Foo bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              passsword: "",
+                                              password_confirmation: ""  } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+  
   # test "the truth" do
   #   assert true
   # end
