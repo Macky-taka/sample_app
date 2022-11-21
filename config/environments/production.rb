4c411baa5855003dd5bf4bc28b434f9058b5ad9f
Macky <makino@smartworx.co.jp>
2022-11-21 08:42:36 +0000

    rivise account_activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,14 +67,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'gruesome-demon-81651.herokuapp.com'
-  config.action_mailer.default_url_options = {host: host}
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port                    => ENV['MAILGUN_SMTP_PORT'],
     :address                 => ENV['MAILGUN_SMTP_SERVER'],
     :user_name               => ENV['MAILGUN_SMTP_LOGIN'],
     :password                => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain                  => host,
-    :authenticated           => :plain,
+    :authentication           => :plain,
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
