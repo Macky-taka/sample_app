931b2022493e4bbda0bbe3be63ca804a34e0e202
Macky <makino@smartworx.co.jp>
2022-11-22 03:40:12 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'rassword_restes/new'
+  get 'rassword_restes/edit'
   get 'sessions/new'
   get 'users/new'
   root 'static_pages#home'
@@ -11,5 +13,6 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
