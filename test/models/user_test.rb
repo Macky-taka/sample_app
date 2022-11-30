adac4680451a7e6566d2daf5080dc20333a663c6
Macky <makino@smartworx.co.jp>
2022-11-30 08:33:43 +0000

    Add user folllowing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -72,6 +72,36 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+  
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+  
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users (:lana)
+    # check the post of following
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # check the own post
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # check the post of unfollowing
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+    
   # test "the truth" do
   #   assert true
   # end
